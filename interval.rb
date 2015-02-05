5fb2fa954b02ad43c4aca2f7f547bc8290ebce68
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-02-04 21:44:42 -0500

    Extract greatest_interval method

--- interval.rb
+++ interval.rb
@@ -1,3 +1,7 @@
+def greatest_interval(observations)
+  generate_intervals(observations).sort_by {|x| -x.values.first }.first
+end
+
 def total(start, finish, diffs)
   sum = 0
   diffs[start..finish].each do |num|
@@ -23,7 +27,8 @@ def distances(observations)
   observations.map { |x| x[1] - x[0]}
 end
 
+
 observations = [[5, 3], [5, 10], [5, 10], [5, 10], [5, 15], [15, 35], [8, 7],
                 [1, 0], [5, 4], [5, 4]]
 
-print generate_intervals(observations).sort_by {|x| x.values.first }.last
+print greatest_interval(observations)
